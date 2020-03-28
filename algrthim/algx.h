40024200747d356e231ee1e6bf27b04212e92aa2
JMacKenzie872 <57580342+JMacKenzie872@users.noreply.github.com>
2020-03-28 14:03:07 -0600

    Create algx.h

---
+++ algrthim/algx.h
@@ -0,0 +1,16 @@
+#ifndef _ALGX_H_
+#define _ALGX_H_
+
+struct cell{
+    struct cell *up;
+    struct cell *down;
+    struct cell *left;
+    struct cell *right;
+};
+
+
+void search(cell header, int depth, int soln[]){
+
+};
+
+#endif
