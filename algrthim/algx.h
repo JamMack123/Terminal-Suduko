7224d02517bebdfc2eaf9cfebacb3eeacc0923b0
JMacKenzie872 <57580342+JMacKenzie872@users.noreply.github.com>
2020-04-02 17:11:01 -0600

    Update algx.h

--- algrthim/algx.h
+++ algrthim/algx.h
@@ -1,16 +1,26 @@
 #ifndef _ALGX_H_
 #define _ALGX_H_
+#include <vector>
+using namespace std;
 
 struct cell{
     struct cell *up;
     struct cell *down;
     struct cell *left;
     struct cell *right;
+    struct cell *column;
+    int rowID;
+    int colID;
+    int count =0;
 };
 
+void createLinkedMatrix(vector<vector<bool>> probMatrix, int MaxRow, int MaxColumn);
 
-void search(cell header, int depth, int soln[]){
+void search(cell *header, int depth, vector<int> soln);
 
-};
+void cover();
+
+void uncover();
+void soln(vector<vector<bool>>);
 
 #endif
