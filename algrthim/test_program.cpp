d51ec4a3c6b2ea31d0ed4a0f21f9d59760588b88
JMacKenzie872 <57580342+JMacKenzie872@users.noreply.github.com>
2020-04-02 17:11:29 -0600

    Update test_program.cpp

--- algrthim/test_program.cpp
+++ algrthim/test_program.cpp
@@ -1,6 +1,30 @@
 #include "algx.h"
-
+#include <vector>
+#include <iostream>
 
 int main(){
+    cell *h = new cell; 
+    vector<vector<bool>> prob;
+    for(int i = 0; i < 10; i++){
+        vector<bool> temp;
+        for(int j=0; j < 10; j++){
+            if(i == 0){
+                temp.push_back(true);
+            }else
+            {
+            temp.push_back(false);
+            }
+        }
+        prob.push_back(temp);
+    }
+    prob[1][1] = true;
+    prob[2][2] = true;
+    prob[3][3] = true;
+    prob[4][4] = true;
+    // prob[6][4] = true;
+    // prob[9][2] = true;
+    // prob[2][8] = true;
+    cell test =  createLinkedMatrix(prob, 10,10);
+    
     return 0;
 }
